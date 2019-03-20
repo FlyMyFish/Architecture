cb7626ff20826834d41be50c424e559cda9d3048
张世琛 <754314442@qq.com>
2019-03-20 15:48:45 +0800

    添加注释，添加umeng的一些配置

--- app/src/main/java/com/shichen/architecture/sport/SportActivity.java
+++ app/src/main/java/com/shichen/architecture/sport/SportActivity.java
@@ -1,10 +1,13 @@
 package com.shichen.architecture.sport;
 
+import android.Manifest;
 import android.widget.TextView;
 
 import com.shichen.architecture.R;
 import com.shichen.architecture.basic.BaseActivity;
 import com.shichen.architecture.basic.Viewable;
+
+import java.util.List;
 
 import butterknife.BindView;
 
@@ -12,7 +15,7 @@ import butterknife.BindView;
  * @author shichen 754314442@qq.com
  * Created by Administrator on 2018/10/12.
  */
-@Viewable(presenter = SportActivityPresenter.class, layout = R.layout.activity_sport)
+@Viewable(presenter = SportActivityPresenter.class, layout = R.layout.activity_sport, needPermissions = {Manifest.permission.READ_PHONE_STATE, Manifest.permission.WRITE_EXTERNAL_STORAGE})
 public class SportActivity extends BaseActivity<SportContract.View, SportActivityPresenter> implements SportContract.View {
     @BindView(R.id.tv_category)
     TextView tvCategory;
@@ -25,5 +28,17 @@ public class SportActivity extends BaseActivity<SportContract.View, SportActivit
     @Override
     public void setCategory(String category) {
         tvCategory.setText(category);
+    }
+
+    @Override
+    public void allPermissionsOk(String[] permissions) {
+        super.allPermissionsOk(permissions);
+        //如果顺利获取到了所有的权限，你的逻辑
+    }
+
+    @Override
+    public void permissionsDenied(List<String> deniedPermissions) {
+        super.permissionsDenied(deniedPermissions);
+        //如果一些权限或者所有权限被禁止，你的逻辑
     }
 }
